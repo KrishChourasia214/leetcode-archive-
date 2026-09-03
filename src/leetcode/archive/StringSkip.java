ed931aeb11165d7e38065b15c8b1aea939766bce
Krish Chourasia <krishchourasia2005@gmail.com>
2026-09-04 03:04:36 +0530

    Rename methods and update string skipping logic

--- src/leetcode/archive/StringSkip.java
+++ src/leetcode/archive/StringSkip.java
@@ -3,19 +3,38 @@ package recursion;
 public class StringSkip {
 
 	public static void main(String[] args) {
-		String str = "bbacbaa" ;
+		String str = "bbachbaa" ;
+		String str2 = "ApplePie" ;
 		StringBuilder sb = new StringBuilder() ;
-		String str1 = stringSkip(str, sb, 'b', 0) ;
+		String str1 = charSkip(str, sb, 'b', 0) ;
 		System.out.println(str1) ;
+		StringBuilder sb1 = new StringBuilder() ;
+		String str3 = stringSkip(str2,"Apple", 0, sb1) ;
+		System.out.println(str3) ;
 
 	}
-	public static String stringSkip(String str, StringBuilder sb,  char s, int i) {
+	// Skip only a character
+	public static String charSkip(String str, StringBuilder sb,  char s, int i) {
 		if(i>=str.length()) {
 			return sb.toString() ;		}
 		if(str.charAt(i)!=s) {
 			sb.append(str.charAt(i)) ;
 		}
-		return stringSkip(str, sb, s, i+1) ;
+		return charSkip(str, sb, s, i+1) ;
 	}
+	// Skip a string i.e. "Apple"
+	public static String stringSkip(String str, String skip, int i, StringBuilder sb) {
+		if(i>=str.length()) {
+			return sb.toString() ;
+		}
+		if(str.startsWith(skip, i)) {
+			return stringSkip(str, skip, i+skip.length(), sb) ;
+		}
+		sb.append(str.charAt(i)) ;
+		return stringSkip(str, skip, i+1, sb) ;
+		
+	}
+	
+	
 
 }
