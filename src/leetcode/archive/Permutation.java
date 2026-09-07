38eca426a983fe7fd33e2ab1424f19d3de653025
Krish Chourasia <krishchourasia2005@gmail.com>
2026-09-07 20:38:50 +0530

    Implement permutation list generation method
    Added a method to generate a list of permutations.

--- src/leetcode/archive/Permutation.java
+++ src/leetcode/archive/Permutation.java
@@ -1,10 +1,11 @@
 package recursion;
-
+import java.util.* ;
 public class Permutation {
 
 	public static void main(String[] args) {
 		String str = "abc" ;
 		permRec("",str) ;
+		System.out.println(permRecList("", "abc")) ;
 	}
 	
 	public static void permRec(String p, String up) {
@@ -20,6 +21,21 @@ public class Permutation {
 			
 		}
 		
+	}
+	public static ArrayList<String> permRecList(String p, String up){
+		if(up.isEmpty()) {
+			ArrayList<String> list = new ArrayList<>() ;
+			list.add(p) ;
+			return list ;
+		}
+		char ch = up.charAt(0) ;
+		ArrayList<String> ans = new ArrayList<>() ;
+		for(int i = 0 ; i<p.length()+1 ; i++) {
+			String f = p.substring(0,i) ;
+			String s = p.substring(i, p.length()) ;
+			ans.addAll(permRecList(f+ch+s, up.substring(1))) ;
+		}
+		return ans ;
 	}
 
 }
